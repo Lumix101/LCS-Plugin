7e58ca6ecc4d7f4d3cc11e1d4e9c7e629ffed627
Marc <marc.ploetzke@gmail.com>
2023-01-15 09:09:56 +0100

    Update HealCommand.java

--- src/main/java/de/lumix/lumcraftserver/commands/HealCommand.java
+++ src/main/java/de/lumix/lumcraftserver/commands/HealCommand.java
@@ -13,10 +13,18 @@ public class HealCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            player.setHealth(20);
-            player.setFoodLevel(20);
-            player.sendMessage(ChatColor.GREEN + "[LCS-MainPlugin] Du hast dich geheielt!");
 
+            if (player.hasPermission("lcs.main.heal")) {
+                if (args.length == 0){
+                    player.setHealth(20);
+                    player.setFoodLevel(20);
+                    player.sendMessage(ChatColor.GREEN + "[LCS-MainPlugin] Du hast dich geheielt!");
+                } else {
+                    player.sendMessage(ChatColor.RED + "[LCS-MainPlugin] Du hast den Befehl falsch ausgeführt! Bitte verwende /heal");
+                }
+            } else {
+                player.sendMessage(ChatColor.RED + "[LCS-MainPlugin] Du hast nicht genug Rechte!");
+            }
         } else {
             sender.sendMessage(ChatColor.BLUE + "[LCS-MainPlugin] Böser Dev! Du kannst den Command nicht in der Console ausfüren!");
         }
